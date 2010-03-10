d3a52590f7925cdc79d7935828e6a1efdd6dae68
Alex Bernier <alexgbernier@gmail.com>
2010-03-10 16:37:24 -0700

    Adding license

--- jquery.jselected.js
+++ jquery.jselected.js
@@ -2,6 +2,10 @@
  * @title jSelected
  * @overview jQuery plugin to persist radio and checkbox state to a parent element.
  * @author Alex Bernier
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
  */
 (function($){
     $.fn.jSelected = function(options){
